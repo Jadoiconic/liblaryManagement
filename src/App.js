8ec1a9cc958dfd24cb0f6d37ba9af554bb9efda5
Mustapha Iconic <86121130+MustaDevPro@users.noreply.github.com>
2022-12-13 17:46:38 +0200

    Initial commit Setup environment
    setting tailwind css

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="bg-slat-700 flex gap-3 py-5">
+      <h1 className="text-3xl font-bold ">
+        Hello world!
+      </h1>
+      <h1 className="text-3xl font-bold bg-[teal] rounded-xl">
+        Hello world!
+      </h1>
+      <h1 className="text-3xl font-bold ">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
