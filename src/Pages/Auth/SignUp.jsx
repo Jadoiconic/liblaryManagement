5931af0e353cf3d072f63af048f380aaa7ff3bac
Mustapha Iconic <86121130+MustaDevPro@users.noreply.github.com>
2022-12-22 23:27:30 +0200

    register form

--- src/Pages/Auth/SignUp.jsx
+++ src/Pages/Auth/SignUp.jsx
@@ -1,13 +1,55 @@
 import React from 'react'
 import FooterSection from '../../Layout/Footer/FooterSection'
 import NavBAr from '../../Layout/header/header'
+import myImage from '../../assets/signBg.PNG'
 
 const SignUp = () => {
     return (
         <>
             <NavBAr />
-            <div className='text-5xl font-bold text-center'>
-                Sign Up Page
+            <div className='flex justify-center items-center w-full bg-zinc-200'>
+                <div className='md:w-3/4 bg-white sm:w-full lg:w-3/4 rounded-md md:my-10 sm:my-5 py-2 px-4 grid md:grid-cols-2'>
+                    <div className='p-2 shadow w-3/4'>
+                        <form action="" method="post">
+                            <h2 className='font-bold text-2xl'>
+                                Create New Account
+                            </h2>
+                            {/* <div className='pt-2'>
+                                <label htmlFor="names" className='font-bold text-sm'>Full Name</label>
+                                <input type="text" className='p-2 w-full border rounded-lg border-gray-300 focus:border-indigo-600 focus:outline-none valid:border-green-500 invalid:border-red-600' required placeholder='you@example.com' />
+                            </div> */}
+                            <div className='pt-2'>
+                                <label htmlFor="email" className='font-bold text-sm'>Email Address</label>
+                                <input type="email" className='p-2 w-full border rounded-lg border-gray-300 focus:border-indigo-600 focus:outline-none valid:border-green-500 invalid:border-red-600' required placeholder='you@example.com' />
+                            </div>
+                            <div className='pt-2'>
+                                <label htmlFor="email" className='font-bold text-sm'>Password</label>
+                                <input type="password" minLength={6} maxLength={16} className='p-2 w-full border border-gray-300 rounded-lg focus:border-indigo-600 focus:outline-none valid:border-green-500 invalid:border-red-600' required placeholder='Passowrd' />
+                            </div>
+                            <div className='pt-2'>
+                                <label htmlFor="email" className='font-bold text-sm'>Confirm Password</label>
+                                <input type="password" minLength={6} maxLength={16} className='p-2 w-full border border-gray-300 rounded-lg focus:border-indigo-600 focus:outline-none valid:border-green-500 invalid:border-red-600' required placeholder='Passowrd' />
+                            </div>
+                            <div className='pt-2'>
+                                <input type="checkbox" id='me' className='mr-1 rounded-sm' />
+                                <label htmlFor="me">Remember me</label>
+                            </div>
+                            <button className='w-full py-2 my-3'>Sign In</button>
+                            <p>Doesn't have account yet? <a href='./register'>Sign Up</a></p>
+                            <div className='relative my-4 mx-auto'>
+                                <div className="border relative"></div>
+                                <span className="absolute text-center mx-[33%] bg-white mt-[-0.9rem]">Or Login with</span>
+                            </div>
+                            <div className='flex justify-around'>
+                                <button className='p-2 w-2/5 bg-[#e02121]'>Google</button>
+                                <button className='p-2 w-2/5 bg-[#127]'>Facebook</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div className='w-full'>
+                        <img src={myImage} className='w-full h-auto' />
+                    </div>
+                </div>
             </div>
             <FooterSection />
         </>
