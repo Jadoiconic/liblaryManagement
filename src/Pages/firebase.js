d618555661f2c580d1b5410f3607433d00bec8a2
Mustapha Iconic <86121130+MustaDevPro@users.noreply.github.com>
2022-12-23 13:35:52 +0200

    firebase configuration

---
+++ src/Pages/firebase.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD8EPkIRAruF7hZ5Ejxk-GUtDE5MBEzccw",
+  authDomain: "learning-188f8.firebaseapp.com",
+  projectId: "learning-188f8",
+  storageBucket: "learning-188f8.appspot.com",
+  messagingSenderId: "227860808148",
+  appId: "1:227860808148:web:ebe41d8e6a626668b4d641"
+};
+
+
+const app = initializeApp(firebaseConfig);
